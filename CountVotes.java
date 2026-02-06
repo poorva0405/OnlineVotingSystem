27f99eb95ef0c93a52b7c633851033af3ce3ec9b
poorva0405 <poorva.damle@cumminscollege.in>
2026-02-06 12:15:19 +0530

    Adding CountVotes module

---
+++ CountVotes.java
@@ -0,0 +1,33 @@
+package OnlineVotingSystem;
+
+import java.util.Map;
+
+public class CountVotes {
+
+    public static void displayResults() {
+        Map<Integer, Integer> votes = CastVote.getVotes();
+
+        if (votes.isEmpty()) {
+            System.out.println("No votes cast yet.");
+            return;
+        }
+
+        for (Map.Entry<Integer, Integer> entry : votes.entrySet()) {
+            System.out.println(
+                AddCandidate.getCandidates().get(entry.getKey())
+                + " : " + entry.getValue() + " votes"
+            );
+        }
+    }
+
+    public static String declareWinner() {
+        return CastVote.getVotes()
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .map(e -> AddCandidate.getCandidates().get(e.getKey()))
+                .orElse("No votes cast");
+    }
+}
+
+
